5bd2c4da9a679d9501dc022de7897c8e7239a56b
dncworking <dominyka.dobuzinskaite@gmail.com>
2025-11-25 17:16:53 +0200

    camelize.js / work done

---
+++ camelize.js
@@ -0,0 +1,13 @@
+"use strict";
+
+function camelize(str) {
+  let words = str.split(" ");
+  for (let i = 0; i < words.length; i++) {
+    words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1);
+  }
+  return words.join("");
+}
+
+console.log(camelize("JavaScript Exercises"));
+console.log(camelize("JavaScript exercises"));
+console.log(camelize("JavaScriptExercises"));
