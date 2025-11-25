4ed88ef8de97419d4567c23cc99edf35c103b31b
dncworking <dominyka.dobuzinskaite@gmail.com>
2025-11-25 16:51:05 +0200

    capitalizeLetter.js / work done

---
+++ capitalizeLetter.js
@@ -0,0 +1,10 @@
+"use strict";
+
+function capitalize_Words(str) {
+  let words = str.split(" ");
+  for (let i = 0; i < words.length; i++) {
+    words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1);
+  }
+  return words.join(" ");
+}
+console.log(capitalize_Words("js string exercises"));
